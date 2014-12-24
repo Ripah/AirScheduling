71c0ea297b26e41fdc2c86f5c59fec1df844d1d7
David Bosch <david.bosch@est.fib.upc.edu>
2014-12-24 18:07:39 +0100

    hola

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Administrador
+ *
+ * Created on 24 de diciembre de 2014, 18:02
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
