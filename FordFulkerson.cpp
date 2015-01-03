2875cbc7018c67bb5c7b10b6c9311aad605c8b0f
MartinCristobal <samfisher454@hotmail.com>
2015-01-03 20:55:25 +0100

    Update FordFulkerson.cpp

--- FordFulkerson.cpp
+++ FordFulkerson.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "FordFulkerson.h"
+#include <iostream>
 
 FordFulkerson::FordFulkerson() {
 }
@@ -13,6 +14,7 @@ FordFulkerson::FordFulkerson() {
 FordFulkerson::FordFulkerson(const FordFulkerson& orig) {
 }
 int FordFulkerson::run(graph &grafo, int s, int t){
+    //std::cout << "FF" << std::endl;
     BFS bfs;
     graph rGraph(grafo.get_size());
     for (int i = 0; i < rGraph.get_size(); ++i) {
@@ -22,15 +24,18 @@ int FordFulkerson::run(graph &grafo, int s, int t){
                 int flux = grafo.get_flux(i,j);
                 int capacity = grafo.get_capacity(i,j);
                 int residual_capacity = capacity - flux;
+                rGraph.add_edge(j, i);
                 rGraph.add_edge(i, j);
                 rGraph.set_capacity(i,j,residual_capacity);
                 rGraph.set_capacity(j,i,flux);
             }
         }
     }
+    //std::cout << "Residual Graph OK" << std::endl;
     int parent[grafo.get_size()];
     int max_flow = 0;
     while(bfs.search(rGraph, s, t, parent)) {
+       // std::cout << "Hay camino" << std::endl;
         int flow = std::numeric_limits<int>::max();
         int u;
         for (int node = t; node != s; node = parent[node]) {
@@ -45,12 +50,22 @@ int FordFulkerson::run(graph &grafo, int s, int t){
             }
             else {
                 rGraph.set_capacity(node, u, rGraph.get_capacity(node, u)+flow);
-                grafo.set_flux(u, node, grafo.get_flux(u, node)-flow);
+                grafo.set_flux(node, u, grafo.get_flux(node, u)-flow);
             }
         }
-        max_flow += flow;
     }
-    return max_flow;
+    int sat = 0;
+    std::list<int> vecinos_s = grafo.get_neighbours(s);
+    for (std::list<int>::iterator it = vecinos_s.begin(); it != vecinos_s.end(); it++) {
+        max_flow += grafo.get_flux(s, (*it));
+        sat += grafo.get_capacity(s, (*it));
+    }
+    std::cout << "Capacidad: " << sat << std::endl;
+    std::cout << "Flujo: " << max_flow << std::endl;
+    if (max_flow == sat) {
+        return max_flow;
+    }
+    else return -1;
 }
 FordFulkerson::~FordFulkerson() {
 }
